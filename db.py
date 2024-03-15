fa5909b77d87b519a802327c712ae96f4b7e4811
georgi-stoyanow <gemast1311@gmail.com>
2024-03-15 10:33:01 +0200

    joji kod sutrin 15.3

---
+++ db.py
@@ -0,0 +1,16 @@
+from website import db
+from website.models import Item
+from main import app 
+
+with app.app_context():
+    i1 = Item(name = "Soup", description = "ingredients: potatoes, carrots, chicken", barcode = "123456789012", price = 7)
+    i2 = Item(name = "Pasta", description = "ingredients: spaghety, tomato souce, parmegano", barcode = "254652562661", price = 11)
+    i3 = Item(name = "Salad", description = "ingredients: lettuce, sauce, tomatoes, cucumbers, olives", barcode = "123456789014", price = 4)
+    i4 = Item(name="Pizza",description="ingredians: peperoni,cheese,tomato sauce,dough", barcode = "123456789015", price = 6)
+    i5 = Item(name="Kebab",description="ingredians: chicken,salad,tomato,tortilla,garlic sauce", barcode = "123456789016", price = 5)
+    db.session.add(i1)
+    db.session.add(i2)
+    db.session.add(i3)
+    db.session.add(i5)
+    db.session.add(i4)
+    db.session.commit()
---
+++ website/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField
+
+
+class PurchaseItemForm(FlaskForm):
+    submit = SubmitField(label='Purchase Item!')
+
+class SellItemForm(FlaskForm):
+    submit = SubmitField(label='Sell Item!')
